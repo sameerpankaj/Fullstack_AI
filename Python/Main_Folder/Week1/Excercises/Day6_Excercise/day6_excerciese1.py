7e6ed4660add5b0b4f6c38a0e1b0149ef549bfcb
Sameer Pankaj <sameer.karnaa@gmail.com>
2026-06-16 21:45:15 +0200

    excercise 6

---
+++ Python/Main_Folder/Week1/Excercises/Day6_Excercise/day6_excerciese1.py
@@ -0,0 +1,16 @@
+#Count Words and Lines in a File
+
+def count_words_and_lines(filename):
+    try:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            line_count = len(lines)
+            word_count = sum(len(line.split()) for line in lines)
+
+            print(f'Number of lines: {line_count}')
+            print(f'Numebr of words: {word_count}')
+
+    except FileNotFoundError:
+        print(f'File {filename} not found')
+
+count_words_and_lines('samples.txt')
---
+++ Python/Main_Folder/Week1/Excercises/Day6_Excercise/problems.py
@@ -0,0 +1,10 @@
+'''
+1) Count Words and Lines in a File
+2) Write and Read a List of Items
+
+
+Additional Pracrtice
+  Write a program to copy the contents of one file to another
+  Create a program that counts the number fo occurrences of a specific word in a text file
+  Write a program to log messages with timestamps into a file
+'''
